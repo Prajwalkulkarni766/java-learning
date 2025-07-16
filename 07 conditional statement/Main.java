958cd5f3d11b8f9d1131ed88fb5bb50dfe79db71
Prajwal Kulkarni <128566438+Prajwalkulkarni766@users.noreply.github.com>
2025-07-16 18:46:48 +0530

    conditional statement

---
+++ 07 conditional statement/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 25;
+
+        if (age >= 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are still a child");
+        }
+    }
+}
