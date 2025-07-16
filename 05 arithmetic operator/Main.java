d3278e1eb26686f343998b89e8210e205cc625a5
Prajwal Kulkarni <128566438+Prajwalkulkarni766@users.noreply.github.com>
2025-07-16 18:33:33 +0530

    arithmetic operator

---
+++ 05 arithmetic operator/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int num1 = 0;
+        int num2 = 10;
+
+        // addition
+        // int result = num1 + num2;
+
+        // subtraction
+        // int result = num1 - num2;
+
+        // multiplication
+        // int result = num1 * num2;
+
+        // division
+        // int result = num1 / num2;
+
+        // reminder = %
+
+        // increment and decrement
+
+        // order of execution - parenthesis, exponent, multiplication, division, addition, subtraction
+
+        System.out.println("Result: ");
+    }
+}
