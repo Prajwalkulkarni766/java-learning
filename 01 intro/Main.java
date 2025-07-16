7b0540372b1cdbbff7fbe9063b09d174030e6f40
Prajwal Kulkarni <128566438+Prajwalkulkarni766@users.noreply.github.com>
2025-07-16 11:30:46 +0530

    intro to java

---
+++ 01 intro/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        // this is single line comment
+
+        /*
+         * this
+         * is 
+         * multi
+         * line 
+         * comment
+         */
+        System.out.println("Hello java");
+    }
+}
