556dc531abd5224a45dbabb286d70eee2471c554
Prajwal Kulkarni <128566438+Prajwalkulkarni766@users.noreply.github.com>
2025-07-16 11:33:09 +0530

    variables

---
+++ 02 variables/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // variables are container for the value
+
+        // types of variable
+        // primitive - a simple value is stored. eg. int, float, double, char, boolean - stack
+        // reference - hold memory address - heap
+
+        // we use two steps for creating variable - declaration, assignment
+
+        int age;
+        double price = 19.50;
+
+        age = 22;
+
+        System.out.println("Age " + age);
+        System.out.println("Price " + price);
+
+    }
+}
