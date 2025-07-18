6db00134fed9374cad77cd2301158a8fd42e4a15
Prajwal Kulkarni <128566438+Prajwalkulkarni766@users.noreply.github.com>
2025-07-18 10:52:07 +0530

    ternary operator

---
+++ 15 ternary operator/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int score = 61;
+
+        String passOrFail = score >= 60 ? "PASS" : "FAIL";
+
+        System.out.println("Result: " + passOrFail);
+    }
+}
